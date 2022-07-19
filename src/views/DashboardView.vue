b9fac4feb095729c139cd0722bd61bd63273bdbc
Novita Guok <novitaguok@gmail.com>
2022-07-20 01:29:21 +0700

    Fixed dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -15,7 +15,9 @@
           <div class="d-flex w-100 align-items-start">
             <img src="@/assets/img/icon-grid.svg" class="me-2" />
             <div class="row text-white fw-bold d-flex">
-              <span class="fs-4 text-left d-flex">8</span>
+              <span class="fs-4 text-left d-flex">{{
+                this.productList.length
+              }}</span>
               <span class="text-left d-flex" style="font-size: 14px"
                 >Total Produk</span
               >
@@ -28,7 +30,9 @@
           <div class="d-flex w-100 align-items-start">
             <img src="@/assets/img/icon-verification.svg" class="me-2" />
             <div class="row fw-bold d-flex">
-              <span class="fs-4 text-left d-flex">8</span>
+              <span class="fs-4 text-left d-flex">{{
+                this.productList.filter((x) => x.isVerification == null).length
+              }}</span>
               <span class="text-left d-flex" style="font-size: 14px"
                 >Menunggu Verifikasi</span
               >
@@ -41,7 +45,11 @@
           <div class="d-flex w-100 align-items-start">
             <img src="@/assets/img/icon-verification-failed.svg" class="me-2" />
             <div class="row fw-bold d-flex">
-              <span class="fs-4 text-left d-flex">8</span>
+              <span class="fs-4 text-left d-flex">{{
+                this.productList.filter(
+                  (x) => !x.isVerification && x.isVerification !== null
+                ).length
+              }}</span>
               <span class="text-left d-flex" style="font-size: 14px"
                 >Gagal Verifikasi</span
               >
@@ -57,7 +65,9 @@
               class="me-2"
             />
             <div class="row fw-bold d-flex">
-              <span class="fs-4 text-left d-flex">8</span>
+              <span class="fs-4 text-left d-flex">{{
+                this.productList.filter((x) => x.isVerification).length
+              }}</span>
               <span class="text-left d-flex" style="font-size: 14px"
                 >Berhasil Verifikasi</span
               >
@@ -70,8 +80,77 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "DashboardView",
+  data() {
+    return {
+      productList: [],
+      data: {},
+    };
+  },
+  created() {
+    this.getProducts();
+    this.getData();
+  },
+  methods: {
+    getProducts() {
+      var config = {
+        method: "get",
+        url: "https://niuniq.herokuapp.com/api/web/niuniq/products",
+        headers: {
+          Cookie: `token=${localStorage.getItem("token")}`,
+        },
+        params: {
+          limit: 1000000,
+        },
+      };
+      axios(config)
+        .then((response) => {
+          this.productList = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getData() {
+      var config = {
+        method: "get",
+        url: "https://niuniq.herokuapp.com/api/web/niuniq/products",
+        headers: {
+          Cookie: `token=${localStorage.getItem("token")}`,
+        },
+      };
+      axios(config)
+        .then((response) => {
+          console.log(`Next >> ${response.data.pagination.next.page}`);
+          this.data = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getProductsByPage(page) {
+      var config = {
+        method: "get",
+        url: `https://niuniq.herokuapp.com/api/web/niuniq/products/?page=${page}`,
+        headers: {
+          Cookie: `token=${localStorage.getItem("token")}`,
+        },
+      };
+      axios(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+          console.log(`Next >> ${response.data.pagination.next.page}`);
+          this.data = response.data;
+          this.productList = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
